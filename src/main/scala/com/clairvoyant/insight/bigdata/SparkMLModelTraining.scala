bd34ec379540cbe510dd60824d7b84582cac2ecc
aravind <aravind.boppana@clairovyantsoft.com>
2019-08-22 16:02:59 -0700

    Changing Training class to avoid predicting after training

--- src/main/scala/com/clairvoyant/insight/bigdata/SparkMLModelTraining.scala
+++ src/main/scala/com/clairvoyant/insight/bigdata/SparkMLModelTraining.scala
@@ -3,7 +3,6 @@ package com.clairvoyant.insight.bigdata
 import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.{DecisionTreeClassifier, LogisticRegression, NaiveBayes, RandomForestClassifier}
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorAssembler, VectorIndexer}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.udf
@@ -57,11 +56,6 @@ object SparkMLModelTraining {
                 .setOutputCol("indexedFeatures")
                 .setMaxCategories(4)
                 .fit(transformed_data)
-
-        // Split the data into training and test sets (30% held out for testing).
-        val Array(trainingData, testData) = transformed_data.randomSplit(Array(0.7, 0.3))
-
-        testData.show()
 
         // Convert indexed labels back to original labels.
         val labelConverter = new IndexToString()
@@ -137,27 +131,11 @@ object SparkMLModelTraining {
         }
 
         // Train model. This also runs the indexers.
-        val model = pipeline.fit(trainingData)
+        val model = pipeline.fit(transformed_data)
 
         model.save(MODEL_SAVING_LOCATION + "/" + ML_CLASSIFIER)
 
-        // Make predictions.
-        val predictions = model.transform(testData)
-
-        predictions.show()
-        LOGGER.info("Count: " + predictions.count().toString)
-
-        val evaluator = new MulticlassClassificationEvaluator()
-                .setLabelCol("indexedFlower")
-                .setPredictionCol("prediction")
-                .setMetricName("accuracy")
-        val accuracy = evaluator.evaluate(predictions)
-
-        println("Accuracy: " + accuracy)
-        println("Test Error = " + (1.0 - accuracy))
-
-
-        //    predictions.filter("predictedLabel != 'setosa'").show()
+        LOGGER.info("Successfully Saved " + ML_CLASSIFIER + " Model")
 
         spark.stop()
 
